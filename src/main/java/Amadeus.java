c04404146527fc4d2f0b4a751be72d0d156cf571
Markneoneo <markneoneo@gmail.com>
2025-02-01 16:49:45 +0800

    Level 1. Echo
    - Made Login and Logoff functions
    - Included Echo function
    - Exits with "bye"

--- src/main/java/Amadeus.java
+++ src/main/java/Amadeus.java
@@ -1,49 +1,84 @@
+import java.util.Scanner; // Methods for input
+
 public class Amadeus
 {
+    private static final String DIVIDER = "=".repeat(100);
+    //region Amadeus Logo
+    private static final String AMADEUS =
+            """
+                                                                                   @
+                                                                                @@@@
+                                                                              @@@@@@
+                                                                           @@@@@@#@@
+                                                                         @@@@@@@@@@@
+                                                                       @@@@@@@  @@ @
+                                                                    @@@@@@@:    @@@@
+                                                                  @@@@@@@       @@@@
+                                                               -@@@@@@#         @@@@
+                                                             @@@@@@@            @@@@
+                                                           @@@@@@@              @@@@
+                                                         @@@@@@=                @@@@
+                                                      @@@@@@@                   @@@@
+                                                   +@@@@@@=                     @@@@
+                                                 @@@@@@@                        @@@@
+                                               @@@@@@@                          @@@@
+                                             @@@@@@#                            @@@@
+                                          @@@@@@@+                              @@@@
+                                       @@@@@@@#        @@@@@@@@@@@@@@@@@@@@@@@  @@@@
+                                     @%#@*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@    @@@@=@@@@@@@@@@@@@@@@@#-
+                                   #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@=+         @@ @@@@@@@@@@@@@@@@%@@@@@@@@
+                                 @@%@@@@                                     @@                       @@@
+                              @@@@@@@@ @@@@@@@@@@@@@@@ @@@@@@@@@@@ @@@@@@@@@@@@ @@@@@@@@@@@@ @@       @@@ @@@@@@@@@@@
+                            @@@#@@@    @@@   @@    @@@         @@@ @@#       @@ @@       @@@ @@       @@@ @@        \s
+                         @@@@@@@@      @@@   @@    @@@ @@@@@@@@@@@ @@@       @@ @@@@@@@@@@@@ @@       @@@ @@@@@@@@@@@
+                       @@@@@@@         @@@   #@    @@@ @@       @@ @@@       @@ @@           @@       @@%          @@
+                     @@@@@@@@@@@@@@@@@@@@@   @@    @@@ @@@@@@@@@@@ @@@@@@@@@@@@ @@@@@@@@@@@@ @@@@@@@@@@@ @@@@@@@@@@@@
+                                                   @@-                                         @@@@@@@
+                                                   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+                                                                                  @@@@@@@@+
+                                                                                @@@@@@@@-
+                                                                                @@@@@@@
+                                                                                @@@@@
+                                                                                @@@
+                                                                                @
+                    """;
+    //endregion
+
+    public static void logIn()
+    {
+        System.out.println(DIVIDER);
+        System.out.println("User ID: Salieri\nLogging in...\nLaunching...\n");
+        System.out.println(AMADEUS);
+        System.out.println("Nice to meet you, I'm Kurisu Makise, a.k.a. Amadeus.");
+        System.out.println("I look forward to working with you.");
+        System.out.println(DIVIDER);
+    }
+
+    public static void logOff()
+    {
+        //System.out.println(DIVIDER);
+        System.out.println("Goodbye. May our timelines converge once more.\nEl Psy Kongroo");
+        System.out.println(DIVIDER);
+    }
+
+    public static void echo()
+    {
+        String input;
+        Scanner in = new Scanner(System.in);
+        do
+        {
+            input = in.nextLine(); // User Input
+            System.out.println(DIVIDER);
+            System.out.println(input);
+            System.out.println(DIVIDER);
+        } while(!(input.equals("bye")));
+        // Log Off at "bye"
+        logOff();
+    }
+
     public static void main(String[] args)
     {
-        String logo =
-         "                                                               @\n"
-        +"                                                            @@@@\n"
-        +"                                                          @@@@@@\n"
-        +"                                                       @@@@@@#@@\n"
-        +"                                                     @@@@@@@@@@@\n"
-        +"                                                   @@@@@@@  @@ @\n"
-        +"                                                @@@@@@@:    @@@@\n"
-        +"                                              @@@@@@@       @@@@\n"
-        +"                                           -@@@@@@#         @@@@\n"
-        +"                                         @@@@@@@            @@@@\n"
-        +"                                       @@@@@@@              @@@@\n"
-        +"                                     @@@@@@=                @@@@\n"
-        +"                                  @@@@@@@                   @@@@\n"
-        +"                               +@@@@@@=                     @@@@\n"
-        +"                             @@@@@@@                        @@@@\n"
-        +"                           @@@@@@@                          @@@@\n"
-        +"                         @@@@@@#                            @@@@\n"
-        +"                      @@@@@@@+                              @@@@\n"
-        +"                   @@@@@@@#        @@@@@@@@@@@@@@@@@@@@@@@  @@@@\n"
-        +"                 @%#@*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@    @@@@=@@@@@@@@@@@@@@@@@#-\n"
-        +"               #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@=+         @@ @@@@@@@@@@@@@@@@%@@@@@@@@\n"
-        +"             @@%@@@@                                     @@                       @@@\n"
-        +"          @@@@@@@@ @@@@@@@@@@@@@@@ @@@@@@@@@@@ @@@@@@@@@@@@ @@@@@@@@@@@@ @@       @@@ @@@@@@@@@@@\n"
-        +"        @@@#@@@    @@@   @@    @@@         @@@ @@#       @@ @@       @@@ @@       @@@ @@         \n"
-        +"     @@@@@@@@      @@@   @@    @@@ @@@@@@@@@@@ @@@       @@ @@@@@@@@@@@@ @@       @@@ @@@@@@@@@@@\n"
-        +"   @@@@@@@         @@@   #@    @@@ @@       @@ @@@       @@ @@           @@       @@%          @@\n"
-        +" @@@@@@@@@@@@@@@@@@@@@   @@    @@@ @@@@@@@@@@@ @@@@@@@@@@@@ @@@@@@@@@@@@ @@@@@@@@@@@ @@@@@@@@@@@@\n"
-        +"                               @@-                                         @@@@@@@\n"
-        +"                               @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n"
-        +"                                                              @@@@@@@@+\n"
-        +"                                                            @@@@@@@@-\n"
-        +"                                                            @@@@@@@\n"
-        +"                                                            @@@@@\n"
-        +"                                                            @@@\n";
-
-        System.out.println("==================================================================================================");
-        System.out.println("Launching...\n" + logo);
-        System.out.println("Hello! I'm Amadeus!");
-        System.out.println("What can I do for you today?\n");
-        System.out.println("==================================================================================================");
-        System.out.println("Farewell! May we cross paths once more!\nEl Psy Kongroo\n");
-        System.out.println("==================================================================================================");
+        logIn();
+        echo();
     }
 }
